5bb7822998f85f73570204e6b5056e984dac5370
yinfante <yarielinfante@gmail.com>
2018-03-12 19:00:47 -0400

    regex validation

--- src/main/java/com/ef/Parser.java
+++ src/main/java/com/ef/Parser.java
@@ -7,7 +7,24 @@ import org.springframework.batch.core.BatchStatus;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Pattern;
 
+/**
+ * It parses web server access log file, loads the log to MySQL and checks if a given IP makes more than a certain number of requests for the given duration.
+ * <p>
+ * It expects the following execution arguments (not in the same order):
+ * * --accesslog location of log file
+ * * --startDate start date time
+ * * --duration time range. It can be "hourly" or "daily"
+ * * --threshold number of requests threshold
+ * </p>
+ * <p>
+ * ex:
+ * --accesslog=/Users/yariel/Downloads/Java_MySQL_Test/access.log --startDate=2017-01-01.13:00:00 --duration=hourly --threshold=100
+ * </p>
+ *
+ * @author yinfante
+ */
 @Slf4j
 public class Parser {
 
@@ -18,7 +35,12 @@ public class Parser {
 
             Map<String, String> argsMap = new HashMap<>();
 
+            Pattern p = Pattern.compile("-{1,2}[A-Za-z]+=.+");
+
             for (String arg : args) {
+                if (!p.matcher(arg).matches()) {
+                    throw new IllegalArgumentException("arguments must follow format --{argument}={value}");
+                }
                 String[] split = arg.split("=");
                 argsMap.put(split[0].trim(), split[1].trim());
             }
@@ -26,7 +48,15 @@ public class Parser {
             String fileUrl = argsMap.get("--accesslog");
             String startDate = argsMap.get("--startDate");
             String duration = argsMap.get("--duration");
-            int threshold = Integer.valueOf(argsMap.get("--threshold"));
+            int threshold;
+
+            try {
+
+                threshold = Integer.valueOf(argsMap.get("--threshold"));
+
+            } catch (NumberFormatException e) {
+                throw new NumberFormatException("threshold must be a number");
+            }
 
             ParserDTO parserDTO = new ParserDTO(fileUrl, startDate, duration, threshold);
 
@@ -34,7 +64,6 @@ public class Parser {
             BatchStatus batchStatus = parserInvoker.invoke(parserDTO);
 
             log.info("Batch status : " + batchStatus);
-
 
         } catch (Exception e) {
             e.printStackTrace();
